96bb27857d1585344d959a4ba2314ca31443588a
ReynaldTM <137812640+ReynaldTM@users.noreply.github.com>
2024-08-03 22:20:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from playsound import playsound
 import time
 
-playsound("digital-alarm-clock-151920.mp3")
+# ANSI, manupulating terminal
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
 
-
-#
 
 def alarm(seconds):
     time_elapsed = 0
 
+    print(CLEAR)
     while time_elapsed < seconds:
         time.sleep(1)
         time_elapsed += 1
@@ -18,7 +19,14 @@ def alarm(seconds):
         minutes_left = time_left // 60  # integer division.  Closest division to the whole number
         seconds_left = time_left % 60  # remainder after division
 
-        print(f"{hours_left:02d}:{minutes_left:02d}:{seconds_left:02d}")
+        print(f"{CLEAR_AND_RETURN}Alarm will sound in: {hours_left:02d}:{minutes_left:02d}:{seconds_left:02d}")
+
+    playsound("digital-alarm-clock-151920.mp3")
 
 
-alarm(3600)
+hours = int(input("How many hours to wait: "))
+minutes = int(input("How many minutes to wait: "))
+seconds = int(input("How many seconds to wait: "))
+total_time = hours * 3600 + minutes * 60 + seconds
+
+alarm(total_time)
